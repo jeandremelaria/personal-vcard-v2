33df6aa1d3f150e8b4734f5f79199fcb9b48c750
Jeandre Melaria <j.melaria@hotmail.com>
2018-12-29 13:25:24 +0100

    Added: gsap animation to preloader

--- src/Components/Preloader/Preloader.js
+++ src/Components/Preloader/Preloader.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { Row , Col } from 'react-materialize';
+import TweenMax from 'gsap';
 
 class Preloader extends Component {
+
+    componentDidMount() {
+        TweenMax.staggerFrom('.preloader__circle', 2, { scale:-1.2, repeat:-1 }, 0.5);
+    }
     render (){
         return(
             <Row className = 'preloader'>
-                <Col s={1} className = 'preloader__circle preloader__circle--purple'></Col>
-                <Col s={1} className = 'preloader__circle preloader__circle--white'></Col>
-                <Col s={1} className = 'preloader__circle preloader__circle--purple'></Col>
+                <Col s={1} className = 'preloader__circle preloader__circle--purple circle_1'></Col>
+                <Col s={1} className = 'preloader__circle preloader__circle--white circle_2' ></Col>
+                <Col s={1} className = 'preloader__circle preloader__circle--purple circle_3'></Col>
             </Row>
         );
     }
