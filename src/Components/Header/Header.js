475f447490425223f498e0dad0630af17b525b5e
Jeandre Melaria <j.melaria@hotmail.com>
2018-12-29 18:56:34 +0100

    Added materialize HTML setup to index.html

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Row, Col, Button, Icon } from 'react-materialize';
+
+const Header = () => {
+    return(
+
+    );
+}
+
+export default Header;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Row ,  Col } from 'react-materialize';
+import { Row,  Col } from 'react-materialize';
+import Header from '../Header/Header';
 
 
 const Home = () => {
     return (
-        <p>Home</p>
+        <Header/>
     );
 }
 
